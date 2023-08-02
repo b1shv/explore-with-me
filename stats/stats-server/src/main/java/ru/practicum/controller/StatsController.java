e2c51c6fce32b2f793c957f6eefd94d709f20e96
b1shv <a.balashovvv@gmail.com>
2023-08-02 17:10:54 +0300

    fix: валидация дат в запросах /stats

--- stats/stats-server/src/main/java/ru/practicum/controller/StatsController.java
+++ stats/stats-server/src/main/java/ru/practicum/controller/StatsController.java
@@ -3,6 +3,7 @@ package ru.practicum.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import ru.practicum.HitDto;
 import ru.practicum.StatsDto;
+import ru.practicum.exception.ValidationException;
 import ru.practicum.service.StatsService;
 
 import javax.validation.Valid;
@@ -19,6 +21,7 @@ import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @Slf4j
@@ -40,7 +43,22 @@ public class StatsController {
                                    @RequestParam(required = false) String[] uris,
                                    @RequestParam(defaultValue = "false") boolean unique) {
         log.debug("GET /stats, params: start={} end={} uris={} unique={}", start, end, uris, unique);
+        LocalDateTime startParsed = parseDateTimeParam(start);
+        LocalDateTime endParsed = parseDateTimeParam(end);
+        if (startParsed.isAfter(endParsed)) {
+            throw new ValidationException("Start must be before end");
+        }
         return statsService.getStats(parseDateTimeParam(start), parseDateTimeParam(end), unique, uris);
+    }
+
+    @ExceptionHandler
+    public Map<String, String> handleNegativeCount(final ValidationException e) {
+        return Map.of(
+                "status", "BAD_REQUEST",
+                "reason", "Incorrectly made request.",
+                "message", e.getMessage(),
+                "timestamp", LocalDateTime.now().toString()
+        );
     }
 
     private LocalDateTime parseDateTimeParam(String param) {
